c125fc4327132468376504bd00bd9a367af80039
Daniele Trapani <daniele.trapani96@gmail.com>
2024-09-06 20:25:30 +0200

    basic seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts('deleting all users...')
+User.destroy_all
+puts('done')
+
+puts('creating users...')
+
+User.create!(
+  {
+    first_name: 'Daniele',
+    last_name: 'Trapani',
+    email: 'daniele@email.com',
+    password: '123456',
+    password_confirmation: '123456',
+    role: 'manager'
+  }
+)
+
+employee = User.create!(
+  {
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'john@email.com',
+    password: '123456',
+    password_confirmation: '123456',
+    role: 'employee'
+  }
+)
+puts('done')
+
+puts('creating entries...')
+Entry.create!(company: 'Tesla', kwh: 12.5, startdate: Date.today, enddate: Date.today, user: employee)
+Entry.create!(company: 'Ford', kwh: 18.5, startdate: Date.today, enddate: Date.today, user: employee)
+Entry.create!(company: 'BMW', kwh: 200.3, startdate: Date.today, enddate: Date.today, user: employee)
+puts('done')
