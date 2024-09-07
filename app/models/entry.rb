4ce0a3fdae7a4df29ee64cd475dcc7a1a02b65a8
Daniele Trapani <daniele.trapani96@gmail.com>
2024-09-07 17:42:35 +0200

    wrapping in manager view + some validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,8 @@
 class Entry < ApplicationRecord
   belongs_to :user
   validates :company, :kwh, :startdate, :enddate, presence: true
+  validates :kwh, numericality: { greater_than: 0 }
+  validates :end_date, comparison: { greater_than: :start_date }
 
   def start_date
     startdate.strftime('%d/%m/%Y')
